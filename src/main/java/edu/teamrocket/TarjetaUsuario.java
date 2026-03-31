c22bd8f5982616f92414c7abb951370992620d93
juan071825 <juanmateoalvarez@gmail.com>
2026-03-31 21:10:01 +0200

    feat(TarjetaUsuario.java): :sparkles: Añadir clase TarjetaUsuario.

---
+++ src/main/java/edu/teamrocket/TarjetaUsuario.java
@@ -0,0 +1,32 @@
+package edu.teamrocket;
+
+public class TarjetaUsuario implements Autenticacion{
+    
+    private final String id;
+    private Boolean activada = false;
+
+    public TarjetaUsuario(String id, Boolean activada){
+        this.id = id;
+        this.activada = activada;
+    }
+
+    public Boolean isActivada(){
+        return this.activada;
+    }
+
+    public void setActivada(Boolean estado){
+        this.activada = estado;
+    }
+
+    public String getId(){
+        return this.id;
+    }
+
+    public String toString(){
+        StringBuilder tarjetaUsuario = new StringBuilder();
+        tarjetaUsuario.append("El estado de la tarjeta " + 
+                              getId() + "es " + isActivada());
+        return tarjetaUsuario.toString();
+    }
+
+}
